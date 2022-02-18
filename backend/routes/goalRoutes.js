a4d5775a0e604b2f3c7fc858824a9cbdbf2b9f07
Vaibhav Tripathi <vaibhavtrip29@gmail.com>
2022-02-17 21:10:19 -0800

    initial rest api for goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const { getGoals, setGoal, updateGoals, deleteGoals } = require('../controllers/goalController')
 
 router.route('/').get(getGoals).post(setGoal)
-
 router.route('/:id').put(updateGoals).delete(deleteGoals)
 
 module.exports = router
